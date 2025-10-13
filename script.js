8cb66af47e27573646c89c990754d6d890a078d5
tookarunya <111236491+tookarunya@users.noreply.github.com>
2025-10-13 22:22:56 +0700

    script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
    //ตั้งเวลาเปิดการลงคะแนนเสียง
-        var future = Date.parse("November 28, 2023 06:00:00");
+        var future = Date.parse("November 28, 2025 06:00:00");
 
    //ตั้งเวลาปิดการลงคะแนนเสียง 
-        var future2 = Date.parse("December 03, 2023 20:00:00");
+        var future2 = Date.parse("December 03, 2025 20:00:00");
         var timeEnd = 0;
         var status =0;
 
